30b7bb07e53eb7d68a6d24d783318cc97fa07dab
Artem Sapegin <artem@sapegin.ru>
2018-04-01 19:09:54 +0200

    Setup exercise style guides

---
+++ src/exercises/exercise01/Readme.md
@@ -0,0 +1,20 @@
+# Creating a generic text component
+
+## Task
+
+Create a components that renders text in different styles:
+
+* Normal text (base font size, base color);
+* Secondary text (base font size, secondary color);
+* Small text (small font size, secondary color);
+* Error message (base font size, error color).
+
+Props are used to change text style.
+
+### Bonus
+
+The user can change a component or an HTML tag that is used to render text.
+
+## Getting started
+
+1. Run Styleguidist: `npm run exercise 1`
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,41 @@
+const fs = require('fs');
 const path = require('path');
-module.exports = {
-	sections: [
+const args = require('node-args');
+const chalk = require('chalk');
+
+// Show an error message and quit
+const die = message => {
+	console.error(chalk.red(message));
+	console.error();
+	process.exit(1);
+};
+
+// styleguidist server --exercise 1 => 01
+const exercise = args.exercise && args.exercise.toString().padStart(2, '0');
+
+const config = {
+	styleguideDir: 'build/styleguide',
+	styleguideComponents: {
+		Wrapper: path.join(__dirname, 'src/Provider'),
+	},
+	// Read examples from Component.md files only, not from Readme.md
+	getExampleFilename: x => x.replace(/\.js$/, '.md'),
+};
+
+if (exercise) {
+	// Load components from an exercise folder
+	const exerciseFolder = path.join(
+		__dirname,
+		`src/exercises/exercise${exercise}`
+	);
+	if (!fs.existsSync(exerciseFolder)) {
+		die(`Exercise ${args.exercise} not found`);
+	}
+
+	config.title = `Exercise ${args.exercise}`;
+	config.components = `${exerciseFolder}/**/*.js`;
+} else {
+	config.sections = [
 		{
 			name: 'Foundation',
 			content: 'docs/Foundation.md',
@@ -13,9 +48,7 @@ module.exports = {
 			name: 'App components',
 			components: 'src/components/app/**/*.js',
 		},
-	],
-	styleguideDir: 'build/styleguide',
-	styleguideComponents: {
-		Wrapper: path.join(__dirname, 'src/Provider'),
-	},
-};
+	];
+}
+
+module.exports = config;
