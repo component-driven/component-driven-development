c2824bc56974a4e9574d9c8580766dcf498364dc
Andrey Okonetchnikov <andrey@okonet.ru>
2018-04-04 15:38:07 +0200

    Fine tune color swatches

--- src/components/foundation/Colors.js
+++ src/components/foundation/Colors.js
@@ -4,28 +4,32 @@ import { Box, Flex } from 'grid-styled';
 import { darken } from 'polished';
 import theme from '../../theme';
 import Text from '../core/Text';
+import Card from '../core/Card';
 
-const Swatch = styled(Box).attrs({
-	p: 4
-})`
+const Swatch = styled.div`
+	width: 100%;
+	height: 100px;
 	background: ${props => props.color};
-	border: 1px solid ${props => darken(0.15, props.color)};
-	border-radius: ${props => props.theme.borderRadius.base};
+	border-bottom: 1px solid ${props => darken(0.15, props.color)};
+	border-top-left-radius: ${props => props.theme.borderRadius.base};
+	border-top-right-radius: ${props => props.theme.borderRadius.base};
 `;
 
 const ColorSample = ({color, children}) => (
-	<Box mr={2}>
+	<Card m={2} p={0} width={1/6}>
 		<Swatch color={color} />
+		<Box p={2}>
 		<Text>{children}</Text>
 		<Text tertiary>{color}</Text>
-	</Box>
+		</Box>
+	</Card>
 )
 
 export default class Colors extends React.Component {
 	render() {
 		const colors = theme.color;
 		return (
-			<Flex>
+			<Flex flexWrap="wrap">
 				{Object.keys(colors).map(key => (
 					<ColorSample color={colors[key]} key={key}>
 						{key}
