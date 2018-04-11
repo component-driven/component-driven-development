3ca8cd4cce3635bfaced149b02a5fe43fb3d2913
Artem Sapegin <artem@sapegin.ru>
2018-04-11 21:57:24 +0200

    Simplify Heading component

--- src/components/core/Heading.js
+++ src/components/core/Heading.js
@@ -1,27 +1,19 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from 'grid-styled';
 import styled from 'styled-components';
 
-const Base = styled.p`
+/**
+ * A text heading.
+ */
+const Heading = styled(Box).attrs({
+	is: props => props.is,
+	m: 0,
+})`
 	font-family: ${props => props.theme.fontFamily.base};
 	font-size: ${props => props.theme.fontSize[props.size]};
 	color: ${props => props.theme.color.base};
 	font-weight: normal;
 `;
-
-const Container = Base.withComponent(Box);
-
-/**
- * A text heading.
- */
-const Heading = ({ children, ...props }) => {
-	return (
-		<Container m={0} {...props}>
-			{children}
-		</Container>
-	);
-};
 
 Heading.propTypes = {
 	/** Custom component or HTML tag */
