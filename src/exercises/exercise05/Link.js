83965395487a980d87da53c90a8437bd967cd963
Artem Sapegin <artem@sapegin.ru>
2018-04-11 21:59:04 +0200

    Update Link example to use Text component

--- src/exercises/exercise05/Link.js
+++ src/exercises/exercise05/Link.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Text from '../../components/core/Text';
 
 /**
  * A link.
  */
-const Link = styled.a``;
+const Link = styled(Text).attrs({ is: 'a' })``;
 
 /** @component */
 export default Link;
