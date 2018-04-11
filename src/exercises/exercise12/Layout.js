95449c61e35b004f81b7a2d7b3d75d838c1f1d02
Artem Sapegin <artem@sapegin.ru>
2018-04-11 22:24:31 +0200

    Add the last example

---
+++ src/exercises/exercise12/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from 'grid-styled';
+import Card from '../../components/core/Card';
+import Footer from '../../components/app/Footer';
+import Heading from '../../components/core/Heading';
+import Page from '../../components/core/Page';
+import DogFinder from '../../components/app/DogFinderContainer';
+import SubscriptionForm from '../../components/app/SubscriptionFormContainer';
+
+const Layout = () => <div>Your code here</div>;
+
+export default Layout;
