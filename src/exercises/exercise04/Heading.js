263e2f2468e84b1353de3068a01501f2f5f3c5c0
Artem Sapegin <artem@sapegin.ru>
2018-06-10 20:46:59 +0200

    Update Heading base code

--- src/exercises/exercise04/Heading.js
+++ src/exercises/exercise04/Heading.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from 'grid-styled';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const headingStyles = css`
+	font-family: ${props => props.theme.fontFamily.heading};
+	font-size: ${props => props.theme.fontSize[props.size]};
+	color: ${props => props.theme.color.base};
+	font-weight: normal;
+`;
 
 /**
  * A text heading.
  */
 const Heading = () => {
-	return <div>Your code here</div>;
+	return <div className={headingStyles}>Your code here</div>;
 };
 
 Heading.propTypes = {
 	/** Custom component or HTML tag */
 	is: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
-	size: PropTypes.oneOf(['alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta'])
-		.isRequired,
+	size: PropTypes.oneOf(['xxlarge', 'xlarge', 'large', 'base', 'small']),
 	children: PropTypes.node,
 };
 
 Heading.defaultProps = {
-	is: 'div',
+	is: 'h1',
+	size: 'xxlarge',
 };
 
 export default Heading;
