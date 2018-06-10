3362ae3375dc0c6942f587e10651831a4d4b0af5
Artem Sapegin <artem@sapegin.ru>
2018-06-10 21:00:58 +0200

    Do not use withComponent

--- src/components/core/Text/Text.js
+++ src/components/core/Text/Text.js
@@ -2,7 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const TextBase = styled.p`
+const Base = ({ is: Component, ...props }) => <Component {...props} />;
+
+/**
+ * A component to render all text in the app.
+ */
+const Text = styled(Base)`
 	margin: 0;
 	font-family: ${props => props.theme.fontFamily.base};
 	font-size: ${props =>
@@ -16,14 +21,6 @@ const TextBase = styled.p`
 				'base'
 		]};
 `;
-
-/**
- * A component to render all text in the app.
- */
-const Text = ({ children, is, ...props }) => {
-	const Tag = TextBase.withComponent(is);
-	return <Tag {...props}>{children}</Tag>;
-};
 
 Text.propTypes = {
 	/** Custom component or HTML tag */
