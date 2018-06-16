a61226c1d6a6ad45dde2cf31bed416c239c512ab
Artem Sapegin <artem@sapegin.ru>
2018-06-16 18:41:55 +0200

    Working base code

--- src/exercises/exercise04/Heading.js
+++ src/exercises/exercise04/Heading.js
@@ -5,19 +5,21 @@ import styled from 'styled-components';
  * A text heading.
  */
 const Heading = styled.h1`
+	font-family: ${props => props.theme.fontFamily.heading};
 	/* Your styles here */
 `;
 
 Heading.propTypes = {
 	/** Custom component or HTML tag */
-	is: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+	// is: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
 	size: PropTypes.oneOf(['xxl', 'xl', 'l', 'base']),
 	children: PropTypes.node,
 };
 
 Heading.defaultProps = {
-	is: 'h1',
+	// is: 'h1',
 	size: 'xxl',
 };
 
+/** @component */
 export default Heading;
--- src/exercises/exercise04/Heading.md
+++ src/exercises/exercise04/Heading.md
@@ -1,6 +1,6 @@
 ```jsx
-<Heading size="xxl" is="h1">Heading 1</Heading>
-<Heading size="xl" is="h2">Heading 2</Heading>
-<Heading size="l" is="h3">Heading 3</Heading>
-<Heading size="base" is="h4">Heading 4</Heading>
+<Heading size="xxl">Heading 1</Heading>
+<Heading size="xl">Heading 2</Heading>
+<Heading size="l">Heading 3</Heading>
+<Heading size="base">Heading 4</Heading>
 ```
