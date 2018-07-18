0e732042ff2258423702afa67f80e608ed7de98d
Artem Sapegin <artem@sapegin.ru>
2018-07-18 20:49:07 +0200

    Tweak 4th exercise

--- src/components/core/TruncatedText/TruncatedText.js
+++ src/components/core/TruncatedText/TruncatedText.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { ellipsis } from 'polished';
 
 const Base = ({ is: Component, ...props }) => <Component {...props} />;
 
 /**
  * Truncate text with an ellipsis.
  */
-const TruncatedText = styled(Base)`
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
+const TruncatedText = styled(Base).attrs({
+	title: props => props.children,
+})`
+	${props => ellipsis(props.width)};
 `;
 
 TruncatedText.propTypes = {
+	width: PropTypes.number,
 	/** Custom component or HTML tag */
 	is: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
 };
