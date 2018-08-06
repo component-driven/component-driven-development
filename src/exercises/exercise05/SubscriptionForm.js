cf03b80cfa8014a8e3ef14030fcc5da03099b5a9
Artem Sapegin <artem@sapegin.ru>
2018-08-06 20:51:44 +0200

    SubscriptionForm base files

---
+++ src/exercises/exercise05/SubscriptionForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Flex, Box } from 'grid-styled';
+import Button from '../../components/core/Button';
+import Input from '../../components/core/Input';
+
+const SubscriptionForm = () => <form>{/* Your code here */}</form>;
+
+export default SubscriptionForm;
