31500f5467bc0bb53d1e1182cfa238afaed0896f
Artem Sapegin <artem@sapegin.ru>
2018-09-07 22:30:02 +0200

    Remove extra margin on body

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { unregister } from './registerServiceWorker';
+import './styles';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
---
+++ src/styles.js
@@ -0,0 +1,8 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+	body {
+		margin: 0;
+		padding: 0;
+	}
+`;
