867215b8d1567cea8d7eb4b0c5063eecf1701b2b
Artem Sapegin <artem@sapegin.ru>
2018-09-07 22:01:32 +0200

    Use Stack Styled

--- src/components/app/Dogs.js
+++ src/components/app/Dogs.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Flex, Box } from 'grid-styled';
+import Stack from 'stack-styled';
 import Dog from './Dog';
 
 const Dogs = ({ dogs }) => (
-	<Flex m={-2} flexWrap="wrap" justifyContent="space-evenly">
-		{dogs.map(dog => (
-			<Box key={dog.id} p={2} width={[1, 1 / 2, 1 / 3, 1 / 4]}>
-				<Dog dog={dog} />
-			</Box>
-		))}
-	</Flex>
+	<Stack gap={3} minWidth={200}>
+		{dogs.map(dog => <Dog key={dog.id} dog={dog} />)}
+	</Stack>
 );
 
 Dogs.propTypes = {
--- src/components/app/Dogs.md
+++ src/components/app/Dogs.md
@@ -3,7 +3,10 @@
   dogs={[
     { id: 'saluki', name: 'Saluki' },
     { id: 'afghan-hound', name: 'Afghan Hound' },
-    { id: 'english-setter', name: 'English Setter' }
+    { id: 'english-setter', name: 'English Setter' },
+    { id: 'azawakh', name: 'Azawakh' },
+    { id: 'basset-hound', name: 'Basset Hound' },
+    { id: 'borzoi', name: 'Borzoi' }
   ]}
 />
 ```
