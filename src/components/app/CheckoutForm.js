2dda7bae9ad5ebf35c7b73a5521a191381d73fe1
Artem Sapegin <artem@sapegin.ru>
2019-02-06 21:06:10 +0100

    Add autocomplete props

--- src/components/app/CheckoutForm.js
+++ src/components/app/CheckoutForm.js
@@ -11,58 +11,85 @@ const CheckoutForm = ({ disabled }) => (
 	<form>
 		<Fieldset label="Delivery" mb={5}>
 			<Widget label="Address" mb={4}>
-				<Input name="address" fullWidth />
+				<Input
+					name="address"
+					autocomplete="shipping street-address"
+					required
+					fullWidth
+				/>
 			</Widget>
 			<Flex flexWrap="wrap" m={-3} mb={3}>
 				<Box width={[1, 1 / 4]} p={3}>
 					<Widget label="Zip code">
-						<Input name="zip" fullWidth />
+						<Input
+							name="zip"
+							autocomplete="shipping postal-code"
+							required
+							pattern="^[0-9]+$"
+							fullWidth
+						/>
 					</Widget>
 				</Box>
 				<Box width={[1, 1 / 4]} p={3}>
 					<Widget label="Country">
-						<Select name="country" fullWidth>
+						<Select
+							name="country"
+							autocomplete="shipping country"
+							required
+							fullWidth
+						>
 							<option>Berlin</option>
 						</Select>
 					</Widget>
 				</Box>
 				<Box width={[1, 1 / 2]} p={3}>
 					<Widget label="City">
-						<Input name="city" fullWidth />
+						<Input
+							name="city"
+							autocomplete="shipping locality"
+							required
+							fullWidth
+						/>
 					</Widget>
 				</Box>
 			</Flex>
 			<Flex flexWrap="wrap" m={-3} mb={3}>
 				<Box width={[1, 1 / 2]} p={3}>
 					<Widget label="Date from">
-						<Input name="datefrom" fullWidth />
+						<Input name="datefrom" type="date" required fullWidth />
 					</Widget>
 				</Box>
 				<Box width={[1, 1 / 2]} p={3}>
 					<Widget label="Date to">
-						<Input name="dateto" fullWidth />
+						<Input name="dateto" type="date" required fullWidth />
 					</Widget>
 				</Box>
 			</Flex>
 		</Fieldset>
 		<Fieldset label="Payment" mb={5}>
 			<Widget label="Card number" mb={4}>
-				<Input name="ccnumber" fullWidth />
+				<Input name="cardnumber" autocomplete="cc-number" required fullWidth />
 			</Widget>
 			<Flex flexWrap="wrap" m={-3} mb={3}>
 				<Box width={[1, 1 / 2]} p={3}>
 					<Widget label="Name on card">
-						<Input name="ccname" fullWidth />
+						<Input name="ccname" autocomplete="cc-name" required fullWidth />
 					</Widget>
 				</Box>
 				<Box width={[1, 1 / 4]} p={3}>
 					<Widget label="Expiry date">
-						<Input name="ccdate" placeholder="MM/YY" fullWidth />
+						<Input
+							name="cc-exp"
+							autocomplete="cc-exp"
+							placeholder="MM/YY"
+							required
+							fullWidth
+						/>
 					</Widget>
 				</Box>
 				<Box width={[1, 1 / 4]} p={3}>
 					<Widget label="Security code">
-						<Input name="cccode" fullWidth />
+						<Input name="cvc" autocomplete="cc-csc" required fullWidth />
 					</Widget>
 				</Box>
 			</Flex>
