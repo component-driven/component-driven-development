d0e86ca78642a93d28e4c61ee330166fa8b72279
Artem Sapegin <asapegin@wayfair.com>
2019-03-09 17:39:05 +0100

    Make the button the same height as input

--- src/components/core/Button/Button.js
+++ src/components/core/Button/Button.js
@@ -10,6 +10,7 @@ const getBgColor = variant => ({ primary: 'primary' }[variant]);
  */
 const Button = styled.button`
 	display: ${props => props.fullWidth && 'block'};
+	height: 2.5rem;
 	padding: ${themeGet('space.3')} ${themeGet('space.4')};
 	border: 1px solid ${themeGet('colors.primary')};
 	border-radius: ${themeGet('radii.base')};
