c2329a25025c3353aa2d9daabfbb900d3a513553
Andrey Okonetchnikov <andrey@okonet.ru>
2019-04-03 22:58:05 +0200

    Use Card component in DogCard and checkout page
    Drop Panel component

--- src/components/app/OrderSummary.js
+++ src/components/app/OrderSummary.js
@@ -2,10 +2,10 @@ import React from 'react';
 import differenceInDays from 'date-fns/difference_in_days';
 import parse from 'date-fns/parse';
 import { Flex, Box } from '@rebass/grid';
-import Image from '../core/Image';
 import Heading from '../core/Heading';
 import Text from '../core/Text';
 import DogInfo from '../app/DogInfo';
+import Card from '../core/Card';
 
 const getDays = (from, to) => {
 	if (!from || !to) {
@@ -29,22 +29,22 @@ const OrderSummary = ({ dog, dateFrom, dateTo }) => {
 			<Heading size="l" as="h3" mb={4}>
 				Order Summary
 			</Heading>
-			<Box mb={4}>
-				<Image src={`/images/${dog.image}`} alt={dog.name} />
-			</Box>
-			<Box mb={4}>
-				<DogInfo dog={dog} />
-			</Box>
-			<Flex>
-				{days && <Text>{days} days</Text>}
-				<Box ml="auto">
-					{summ && (
-						<Heading as="p" size="l">
-							€ {summ}
-						</Heading>
-					)}
-				</Box>
-			</Flex>
+			<Card>
+				<Card.Image src={`/images/${dog.image}`} alt={dog.name} />
+				<Card.Body>
+					<DogInfo dog={dog} />
+					<Flex>
+						{days && <Text>{days} days</Text>}
+						<Box ml="auto">
+							{summ && (
+								<Heading as="p" size="l">
+									€ {summ}
+								</Heading>
+							)}
+						</Box>
+					</Flex>
+				</Card.Body>
+			</Card>
 		</>
 	);
 };
