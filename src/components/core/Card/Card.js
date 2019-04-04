2899f69c07ab01eaca184416114a56a9cbfe9a78
Artem Sapegin <asapegin@wayfair.com>
2019-04-04 21:47:21 +0200

    Simplify Card a bit

--- src/components/core/Card/Card.js
+++ src/components/core/Card/Card.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@rebass/grid';
-import Stack from 'stack-styled';
 import { color, border, borderColor, borderRadius } from 'styled-system';
 import styled from 'styled-components';
 import Image from '../Image';
 
-const CardStack = styled(Stack)`
+const Card = styled(Box).attrs({
+	bg: 'bg',
+	border: 'thin',
+	borderColor: 'light',
+	borderRadius: 'base',
+})`
 	${color};
 	${border};
 	${borderColor};
 	${borderRadius};
+	list-style: none;
 `;
-
-function Card({ children, ...rest }) {
-	return (
-		<CardStack
-			{...rest}
-			gap={0}
-			bg="bg"
-			border="thin"
-			borderColor="light"
-			borderRadius="base"
-		>
-			{children}
-		</CardStack>
-	);
-}
 
 Card.propTypes = {
 	children: PropTypes.node, // We could validate children of specific type here
@@ -35,8 +25,5 @@ Card.propTypes = {
 Card.Image = props => <Image {...props} />;
 Card.Body = ({ children }) => <Box p={4}>{children}</Box>;
 
-/**
- * Media object
- * @component
- */
+/** @component */
 export default Card;
