9500fc3dc821150a8c19ae68f1035d0f69f6179e
Artem Sapegin <asapegin@wayfair.com>
2019-04-19 10:59:41 +0200

    Fix PropTypes

--- src/components/core/Heading/Heading.js
+++ src/components/core/Heading/Heading.js
@@ -20,7 +20,7 @@ const Heading = styled.h1.attrs({
 Heading.propTypes = {
 	/** Custom component or HTML tag */
 	as: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
-	size: PropTypes.oneOf(['xxxl', 'xxl', 'xl', 'l', 'm']),
+	size: PropTypes.oneOf(['xxxl', 'xxl', 'xl', 'l', 'm', 'base']),
 	align: PropTypes.oneOf(['left', 'center', 'right']),
 	children: PropTypes.node,
 };
