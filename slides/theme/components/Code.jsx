9ff326a5462261c0ff85dd970f87bdf08463d1a7
Andrey Okonetchnikov <andrey@okonet.ru>
2019-11-07 13:46:04 +0100

    Feat: Better Code component

--- slides/theme/components/Code.jsx
+++ slides/theme/components/Code.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Prism } from 'react-syntax-highlighter';
+import styled from '@emotion/styled'
 import codeTheme from '../codeTheme';
 
-function Code({ children, ...props }) {
-	console.log(children);
+function Code({ children, language, inline = false }) {
 	if (typeof children !== 'string') {
 		return (
 			<pre style={{ color: 'firebrick' }}>
@@ -13,7 +13,7 @@ function Code({ children, ...props }) {
 		);
 	}
 	return (
-		<Prism {...codeTheme} {...props}>
+		<Prism PreTag={styled.pre({display: inline ? "inline-block" : "block"})} {...codeTheme} useInlineStyles language={language}>
 			{children.replace(/\t/, '  ')}
 		</Prism>
 	);
@@ -21,6 +21,8 @@ function Code({ children, ...props }) {
 
 Code.propTypes = {
 	children: PropTypes.string,
+	language: PropTypes.oneOf(Prism.supportedLanguages),
+	inline: PropTypes.bool
 };
 
 export default Code;
