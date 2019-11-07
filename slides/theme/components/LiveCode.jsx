c283a6cee7b9569f26a220ff832dd8adec0ba302
Andrey Okonetchnikov <andrey@okonet.ru>
2019-11-07 16:00:00 +0100

    Add Grid, Stack and Group examples

--- slides/theme/components/LiveCode.jsx
+++ slides/theme/components/LiveCode.jsx
@@ -1,13 +1,15 @@
 import { LiveCode as XLiveCode } from 'mdx-deck-live-code';
 import { ThemeProvider } from 'styled-components';
+import Group from 'react-group';
 import Text from '../../../src/components/core/Text';
 import theme from '../../../src/theme';
 import Heading from '../../../src/components/core/Heading';
 import Box from '../../../src/components/core/Box';
-import { Flex } from '../../../src/components/core/Flex/Flex';
-import Stack from 'stack-styled';
+import Flex from '../../../src/components/core/Flex';
 import Input from '../../../src/components/core/Input';
 import Button from '../../../src/components/core/Button';
+import Grid from '../../../src/components/core/Grid';
+import Stack from '../../../src/components/core/Stack';
 
 export default ({ code, size, title }) => {
 	return (
@@ -20,7 +22,9 @@ export default ({ code, size, title }) => {
 				scope: {
 					Box,
 					Flex,
+					Grid,
 					Stack,
+					Group,
 					Text,
 					Heading,
 					Input,
