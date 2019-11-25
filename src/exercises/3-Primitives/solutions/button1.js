ebd420c0625b701cbd2dcfa6cdec86aad5cda1bc
Artem Sapegin <asapegin@wayfair.com>
2019-11-25 21:05:19 +0100

    Load examples from files proof of concept

---
+++ src/exercises/3-Primitives/solutions/button1.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+	padding: 8px 16px;
+	color: #fff;
+	background-color: #8667a8;
+	border: 0;
+`;
+
+/** @component */
+export default Button;
