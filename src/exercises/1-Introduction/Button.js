a97028848c13371eb835fdabca3065f4c215802f
Artem Sapegin <artem@sapegin.ru>
2019-11-30 19:12:54 +0100

    Disable linter for templates

--- src/exercises/1-Introduction/Button.js
+++ src/exercises/1-Introduction/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (props) => <button {...props} />
+const Button = props => <button {...props} />;
 
 Button.propTypes = {
 	/** Button label */
 	children: PropTypes.node,
-}
+};
 
 // This will let react-styleguidist know that we're exporting a React component
 // so it can generate the PropTypes table. This is only needed when used
