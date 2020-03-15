2ec1b389c6a7cb0549eb9aeddc95960ba93d1d21
Andrey Okonetchnikov <andrey@okonet.ru>
2020-03-15 22:20:20 +0100

    Update exercise 5

---
+++ src/exercises/5-Patterns/LoginForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+	Heading,
+	Text,
+	Box,
+	Flex,
+	Input,
+	Link,
+	Button,
+	Stack
+} from '../../components';
+
+export default () => {
+	return <form>Your markup here</form>;
+};
+
