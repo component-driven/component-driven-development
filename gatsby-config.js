d939553899289663badcee63c85a3efb4d2e91a0
Andrey Okonetchnikov <andrey@okonet.ru>
2020-04-12 17:28:36 +0200

    WIP on using gatsby-theme

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	pathPrefix: `/slides`,
+	plugins: [
+		{
+			resolve: 'gatsby-theme-mdx-deck',
+			options: {
+				// source directory
+				contentPath: './slides',
+				// base path for routes generate by this theme
+				basePath: '/slides',
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-compile-es6-packages',
+			options: {
+				modules: ['mdx-deck', 'gatsby-theme-mdx-deck', '@mdx-deck/themes'],
+			},
+		},
+	],
+};
