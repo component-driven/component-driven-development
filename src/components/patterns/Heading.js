69b87aede6ea742433b62a67ef3b6a70e15db12b
Andrey Okonetchnikov <andrey@okonet.ru>
2020-04-13 19:30:06 +0200

    Render Heading as h1 by default

--- src/components/patterns/Heading.js
+++ src/components/patterns/Heading.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Text } from '../';
 
 export default props => {
-	return <Text variant="heading" {...props} />;
+	return <Text variant="heading" as="h1" {...props} />;
 };
