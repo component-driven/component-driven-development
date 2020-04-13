5566a60dfe868460e368c97fd87b7151de339ac8
Andrey Okonetchnikov <andrey@okonet.ru>
2020-04-13 15:10:04 +0200

    Add an example of variants for text and link in the login form

--- src/components/patterns/LoginForm.js
+++ src/components/patterns/LoginForm.js
@@ -12,22 +12,29 @@ import {
 export default () => (
 	<Grid as="form" gridGap={4}>
 		<Heading mb={5}>Login</Heading>
-		<Grid gridGap={[1, 4]} gridTemplateColumns={['1fr', '150px 1fr']}>
+		<Grid gridGap={[1, 4]} gridTemplateColumns={['1fr', '6rem 1fr']}>
 			<Text as="label" display="block">
-				<Box mb={2}>Username</Box>
+				Username
 			</Text>
 			<Input type="text" />
 		</Grid>
-		<Grid gridGap={[1, 4]} gridTemplateColumns={['1fr', '150px 1fr']}>
+		<Grid gridGap={[1, 4]} gridTemplateColumns={['1fr', '6rem 1fr']}>
 			<Text as="label" display="block">
-				<Box mb={2}>Password</Box>
+				Password
 			</Text>
 			<Input type="password" />
 		</Grid>
-		<Grid gridGap={[1, 4]} gridTemplateColumns={['1fr', '150px 1fr']}>
+		<Grid gridGap={[1, 4]} gridTemplateColumns={['1fr', '6rem 1fr']}>
 			<Grid gridColumn={[1, 2]} gridGap={3}>
 				<Button variant="primary">Log in</Button>
 				<Link href="#">Forgot your password?</Link>
+				<Text variant="hint">
+					By using our site you agree to the following{' '}
+					<Link href="/" color="inherit">
+						Terms of Service
+					</Link>
+					.
+				</Text>
 			</Grid>
 		</Grid>
 	</Grid>
