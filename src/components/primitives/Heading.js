1b2aada8b97ae4f976890eb4495b4c0a58c2dd54
Andrey Okonetchnikov <okonet@users.noreply.github.com>
2020-04-14 09:15:26 +0200

    Update src/components/primitives/Heading.js
    Co-Authored-By: Artem Sapegin <artem@sapegin.ru>

--- src/components/primitives/Heading.js
+++ src/components/primitives/Heading.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Text } from '../';
 
 export default React.forwardRef((props, ref) => {
-	return <Text variant="heading" as="h1" ref={ref} {...props} />;
+	return <Text as="h1" {...props} variant="heading" ref={ref} />;
 });
