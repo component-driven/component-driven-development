289def39f15cb85f221b1ecf44ec634bdeebe7d3
Andrey Okonetchnikov <andrey@okonet.ru>
2020-04-14 13:41:37 +0200

    Add global styles overrides for both app and slides

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,10 @@
+export default {
+	styles: {
+		root: {
+			bg: '#fff',
+			margin: 0,
+			fontFamily:
+				'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+		},
+	},
+};
